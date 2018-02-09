c3fe0101b8e68bd9cbae7cf94bc59f464cb99f18
Demetrio Perez Osorio <Demetrio.Osorio@walmartlabs.com>
2018-02-08 19:44:16 -0800

    Import Resources

--- Whack-A-Wae/main.cpp
+++ Whack-A-Wae/main.cpp
@@ -5,12 +5,53 @@
 //  Created by Demetrio Osorio - Vendor on 2/7/18.
 //  Copyright © 2018 Demetrio. All rights reserved.
 //
-
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include "GameManagerComponent.hpp"
+#include "FPSMonitor.hpp"
+#include "SDLManagerComponent.hpp"
+#include "InputManager.hpp"
+
+
+using namespace std;
+
+bool testing = true;
+int test();
+int start();
+
+int main()
+{
+    if (testing)
+    {
+        test();
+    }
+    else
+    {
+        start();
+    }
+    return 0;
+}                                                                                                                                                                                                                                                                                                                                                                                      
+
+int test()
+{
+    SDLManagerComponent::start();
+    GameManagerComponent::setup();
+
+
+    while (!GameManagerComponent::gameOver)
+    {
+        SDLManagerComponent::clear();
+        InputManager::update();
+        GameManagerComponent::update();
+        SDLManagerComponent::present();
+        //FPSMonitor::calculateFramerATE();
+
+    }
     return 0;
 }
 
+int start()
+{
+    return 0;
+}
